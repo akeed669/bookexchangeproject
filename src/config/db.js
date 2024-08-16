c19a2e280432e36bcfd7ed5bc4a0245a1298158f
Akeed Manuideen <akeedmanuideen@gmail.com>
2024-08-16 17:32:12 +0530

    created a docker compose file to separate db from backend, modified github workflows to build docker image and deploy changes to AWS

--- src/config/db.js
+++ src/config/db.js
@@ -8,8 +8,9 @@ const sequelize = new Sequelize(
   process.env.DB_USER,
   process.env.DB_PASSWORD,
   {
-    host: process.env.DB_HOST,
+    host: process.env.DB_HOST || "db",
     dialect: "postgres",
+    port: process.env.DB_PORT || 5432, // Default to 5432 if not set
   }
 );
 
