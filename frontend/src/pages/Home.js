eedf26a24512266cafbb2a1b08acc02a24f66598
Akeed Manuideen <akeedmanuideen@gmail.com>
2024-08-17 23:07:56 +0530

    revert this later - testing

--- frontend/src/pages/Home.js
+++ frontend/src/pages/Home.js
@@ -1,7 +1,20 @@
 import React from "react";
 
 const Home = () => {
-  return <h1>Hello world</h1>;
+  const style = {
+    display: "flex",
+    justifyContent: "center",
+    alignItems: "center",
+    height: "100vh",
+    fontSize: "3rem", // Adjust the font size as needed
+    fontWeight: "bold",
+  };
+
+  return (
+    <div style={style}>
+      <h1>Akeed loves Faiza!</h1>
+    </div>
+  );
 };
 
 export default Home;
