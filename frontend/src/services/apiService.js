2440eb515d38af04e16f018106219876a2780aca
Akeed Manuideen <akeedmanuideen@gmail.com>
2024-08-18 13:46:24 +0530

    updated config so that frontend and backend can connect

--- frontend/src/services/apiService.js
+++ frontend/src/services/apiService.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-const API_URL = "/api";
+const API_URL = process.env.REACT_APP_API_URL || "http://backend:5000/api";
 
 axios.interceptors.request.use(
   (config) => {
